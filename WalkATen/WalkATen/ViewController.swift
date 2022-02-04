eedbb6967e891c15415f031b28eb1497d07ea0b7
jboro <joepboro@gmail.com>
2022-02-04 21:24:22 +1100

    can detect when user moved 10 meters

---
+++ WalkATen/WalkATen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalkATen
+//
+//  Created by jboro on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
