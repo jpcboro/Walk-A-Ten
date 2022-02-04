8d4830b26b9620462c7a1574a9c2d02d4616da56
jboro <joepboro@gmail.com>
2022-02-04 21:24:56 +1100

    add gitignore file

--- WalkATen/WalkATen/ViewController.swift
+++ WalkATen/WalkATen/ViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var locationMngr = CLLocationManager()
+    var currentUserLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationMngr.delegate = self
+        locationMngr.desiredAccuracy = kCLLocationAccuracyBest
+        locationMngr.distanceFilter = 10
+        locationMngr.requestLocation()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        requestUserLocation()
     }
 
-
+    func requestUserLocation()
+    {
+        if isLocationServicesAllowed() {
+            locationMngr.startUpdatingLocation()
+        }else{
+            locationMngr.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func isLocationServicesAllowed() -> Bool{
+        if CLLocationManager.locationServicesEnabled(){
+            switch (CLLocationManager.authorizationStatus()){
+            case .notDetermined, .restricted, .denied:
+                return false
+            case .authorizedAlways, .authorizedWhenInUse:
+                return true
+            @unknown default:
+                return false
+            }
+        }
+        return false
+    }
+    
+    func showAlert()
+    {
+        var dialogAlert = UIAlertController(title: "Alert!", message: "You have moved more than 10 meters, congratulations!", preferredStyle: .alert)
+        
+        let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        dialogAlert.addAction(okButton)
+        
+        self.present(dialogAlert, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let currentUserLocation = locations.last{
+            print("Current location: \(currentUserLocation)")
+            showAlert()
+        }
+     
+    }
+    
+}
